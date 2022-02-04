a5f48f79e4c9701491985aa7b7bec2f2d184c8f7
yiri1 <nanuya1234@naver.com>
2022-02-04 16:20:46 +0900

    두번째 그린복지재단
    !

---
+++ 02/js/script.js
@@ -0,0 +1,11 @@
+$(".gnb").hover(
+    function(){
+        $(".sub").stop().slideDown();
+        $(".nav_back").stop().slideDown();
+    },
+    function(){
+        $(".sub").stop().slideUp();
+        $(".nav_back").stop().slideUp();
+    }
+);
+$("")
